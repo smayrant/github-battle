f7903f469a487ce864ace4dc4be77e02d19345b9
Sheldrick <mrmayrant@yahoo.com>
2018-12-03 03:32:22 -0500

    added the functionality to retrieve data from the Github API

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import api from '../utils/api';
 
 function SelectLanguage(props) {
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
     return (
-
         <ul className='languages'>
             {languages.map(lang => {
                 return (
@@ -19,6 +19,18 @@ function SelectLanguage(props) {
     )
 }
 
+function RepoGrid(props) {
+    return (
+        <ul className="popular-list">
+            {props.repos.map(function (repo, index) {
+                <li key={repo.name} className="popular-item">
+                    <div className="popular-rank">#{index + 1}</div>
+                </li>
+            })}
+        </ul>
+    )
+}
+
 SelectLanguage.propTypes = {
     selectedLanguage: PropTypes.string.isRequired,
     onSelect: PropTypes.func.isRequired
@@ -29,22 +41,39 @@ class Popular extends React.Component {
         super(props);
         // the initially selected language will default to 'All'
         this.state = {
-            selectedLanguage: 'All'
+            selectedLanguage: 'All',
+            repos: null
         }
         this.updateLanguage = this.updateLanguage.bind(this);
     }
 
+    // once the component mounts, the updateLanguage method is called with an initial language of 'All' passed in
+    componentDidMount() {
+        this.updateLanguage(this.state.selectedLanguage)
+    }
+
+    // the selected language is updated the what the user clicks on in addition to the fetchPopularRepos function being called on the clicked on language
     updateLanguage(lang) {
         this.setState({
-            selectedLanguage: lang
-        })
+            selectedLanguage: lang,
+            repos: null
+        });
+
+        api.fetchPopularRepos(lang)
+            .then(function (repos) {
+                this.setState(function () {
+                    return {
+                        repos: repos
+                    }
+                })
+            }.bind(this))
     }
 
     render() {
-        const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
         return (
             <div>
                 <SelectLanguage selectedLanguage={this.state.selectedLanguage} onSelect={this.updateLanguage} />
+                <RepoGrid repos={this.state.repos} />
             </div>
         )
     }
